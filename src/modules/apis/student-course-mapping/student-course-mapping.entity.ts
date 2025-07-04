44b306fa6d8342b95663ae97c5eb0fc3b1ad1f6b
rajveer1301 <rajveer@pw.live>
2025-07-04 18:37:10 +0530

    Added FK to student course mapping and added schema.sql for raw tables generation

--- src/modules/apis/student-course-mapping/student-course-mapping.entity.ts
+++ src/modules/apis/student-course-mapping/student-course-mapping.entity.ts
@@ -1,5 +1,15 @@
 import { IdGenerator } from 'src/common/Idgenerator';
-import { Column, Entity, Index, PrimaryColumn, Unique } from 'typeorm';
+import {
+  Column,
+  Entity,
+  Index,
+  PrimaryColumn,
+  Unique,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+import { Students } from '../students/students.entity';
+import { Courses } from '../courses/courses.entity';
 
 @Entity('student_course_mapping')
 @Unique('student_course_unique_contraint', ['student_id', 'course_id'])
@@ -10,9 +20,13 @@ export class StudentCourseMapping {
 
   @Column()
   @Index()
+  @ManyToOne(() => Students, { onDelete: 'CASCADE' })
+  @JoinColumn({ name: 'student_id', referencedColumnName: 'unique_id' })
   student_id: string;
 
   @Column()
   @Index()
+  @ManyToOne(() => Courses, { onDelete: 'CASCADE' })
+  @JoinColumn({ name: 'course_id', referencedColumnName: 'unique_id' })
   course_id: string;
 }
